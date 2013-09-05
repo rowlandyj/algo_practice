3fc0780f0907bb897eb473f38abe4e0239b8a090
Rowland J <rowlandyj@gmail.com>
2013-09-05 15:22:47 -0700

    working on file-addition codeeval

---
+++ file_int_sum.rb
@@ -0,0 +1,5 @@
+def file_int_sum
+	File.open(ARGV[0]).each_line do |line|
+		
+	end
+end
--- multi_table.rb
+++ multi_table.rb
@@ -1,4 +1,19 @@
 def print_multi_table(row, col)
+	i = 1
+
+	while i <= row
+		s = ""
+		j = 1
+
+		while j <= col
+			s << "#{i * j} ".rjust(4)
+			j += 1
+		end
+
+		puts s 
+		i += 1
+	end
+	
 end
 
 print_multi_table(12,12)
